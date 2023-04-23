39300ff865c60bd3e67b581973be2a54485037de
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-04-23 15:58:16 -0300

    Add setup

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eux
+
+sudo yum install -y wget unzip libX11 git procps-ng
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum install -y google-chrome-stable_current_x86_64.rpm
+rm google-chrome-stable_current_x86_64.rpm
+
+CHROME_DRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install --global yarn
+
+git clone https://github.com/aviggiano/selfgpt
+cd selfgpt
+yarn install
+
+screen -d -m -L yarn start
